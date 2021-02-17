0ff608fb68e5b98d6d29c80c63c547028719b6b0
Jose-Verdu_Diaz <pepv@hotmail.es>
2021-02-17 01:41:25 +0100

    Project browser

--- poly2pixel.py
+++ poly2pixel.py
@@ -1,10 +1,15 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QLabel, QGridLayout
+from PyQt5.QtGui import QPixmap
+import glob
 
 from poly2pixel_ui import *
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+
+    projectDir = ""
+
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self) 
@@ -12,28 +17,23 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.browseBtn.setText("Presióname")
         self.browseBtn.clicked.connect(self.openFileNameDialog)
 
+        self.show()
+
     def openFileNameDialog(self):
-      dlg = QFileDialog()
-      dlg.setFileMode(QFileDialog.DirectoryOnly)
-		
-      if dlg.exec_():
-         filenames = dlg.selectedFiles()
-         print(filenames[0])
-    
-    def openFileNamesDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
-        if files:
-            print(files)
-    
-    def saveFileDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
-        if fileName:
-            print(fileName)
-            
+        dlg = QFileDialog()
+        dlg.setFileMode(QFileDialog.DirectoryOnly)		
+        if dlg.exec_():
+            filenames = dlg.selectedFiles()
+            print(filenames[0])
+            projectDir = filenames[0]
+
+            image_list = []
+
+            for filename in glob.glob(projectDir + '/*.jpg'):
+                im = QPixmap(filename)
+                image_list.append(im)
+                self.label.setPixmap(im)
+                
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     window = MainWindow()
